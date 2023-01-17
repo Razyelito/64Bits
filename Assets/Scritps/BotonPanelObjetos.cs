bc6493552046bcad47b87f04fb80bcfc7873d890
AlfredoCalle <alfrcall@espol.edu.ec>
2023-01-17 18:27:08 -0500

    Agregado mostrar panel con tecla P

--- Assets/Scritps/BotonPanelObjetos.cs
+++ Assets/Scritps/BotonPanelObjetos.cs
@@ -6,22 +6,52 @@ public class BotonPanelObjetos : MonoBehaviour
 {
     public GameObject panel;
     public GameObject btn_mostrar;
+    public GameObject mensaje;
+    public static bool esPrimeraVez = true;
+    public static bool activoMensaje = true;
 
     void Start() {
         panel = GameObject.Find("Contenedor_panel");
         btn_mostrar = GameObject.Find("Button_mostrar_panel");
+        mensaje = GameObject.Find("Contenedor_mensaje");
         panel.SetActive(false);
+        mensaje.SetActive(false);
 
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.P) && activoMensaje) {
+            panel.SetActive(true);
+            btn_mostrar.SetActive(false);
+            activoMensaje = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.P) && !activoMensaje)
+        {
+            panel.SetActive(false);
+            btn_mostrar.SetActive(true);
+            activoMensaje = true;
+            if (!esPrimeraVez) {
+                mensaje.SetActive(false);
+            }
+        }
     }
 
     public void cerrarPanel() {
         panel.SetActive(false);
         btn_mostrar.SetActive(true);
+        if (activoMensaje) {
+            mensaje.SetActive(false);
+        }
     }
 
     public void mostrarPanel()
     {
         panel.SetActive(true);
         btn_mostrar.SetActive(false);
+        if (esPrimeraVez) {
+            mensaje.SetActive(true);
+            esPrimeraVez = false;
+        }
+        
     }
 }
